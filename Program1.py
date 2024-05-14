4537af93599cdef1acf04f1621044c5647b7e25d
Robert Antony <antonyrobert202@gmail.com>
2024-05-14 21:54:57 +0530

    learnt about assert

--- Program1.py
+++ Program1.py
@@ -1,6 +1,11 @@
 class item:
     #this is a magic function which excuted when the specific class is created and it is mainly used to define the values in it
-    def __init__(self, name, price, quantity):
+    def __init__(self, name: str , price:float, quantity=0):
+        # validation
+        assert price >=0, f"price {price} must be greater than 0"
+        assert quantity >=0, f"quantity {quantity} must be greater than 0"
+
+        #object
         self.name=name
         self.price=price
         self.quantity=quantity
@@ -9,9 +14,9 @@ class item:
         
 
 
-item1=item("phone", 100, 5)
+item1=item("phone", 2000, -3)
 
-item2=item("laptop",1000,3)
+item2=item("laptop",200,3)
 print(item1.calculate_total_price())
 print(item2.calculate_total_price())
 
---
+++ assert.py
@@ -0,0 +1,4 @@
+x=-5
+assert x>0, "x is not positive"
+if x>0:
+    print("x is positive")
