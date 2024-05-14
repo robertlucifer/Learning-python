330b8792382904b702f1c087f08a3f7679d4e2d7
Robert Antony <antonyrobert202@gmail.com>
2024-05-14 22:45:20 +0530

    learnt about class attribute

--- Program1.py
+++ Program1.py
@@ -1,4 +1,6 @@
 class item:
+    name="Hello" #This is a class attribute where it is shared by all the instance (means all the the way you use this class like a datatype) this is constant and you can use it anywhere in this class it can be also updated outside the class by using the class name
+
     #this is a magic function which excuted when the specific class is created and it is mainly used to define the values in it
     def __init__(self, name: str , price:float, quantity=0):
         # validation
@@ -14,10 +16,12 @@ class item:
         
 
 
-item1=item("phone", 2000, -3)
+item1=item("phone", 2000, 3)
 
 item2=item("laptop",200,3)
 print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+print(item.name)
+item.name="Hello World"
+print(item.name)
 
 
