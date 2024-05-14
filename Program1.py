816edf5311aa42aa8be96638a9602b9f25a536e6
Robert Antony <antonyrobert202@gmail.com>
2024-05-14 18:55:46 +0530

    added the class and understood creating the method and self function

--- Program1.py
+++ Program1.py
@@ -1,19 +1,18 @@
 class item:
-    def calculate_total_price(self, x , y):
-        return x*y
+    #this is a magic function which excuted when the specific class is created and it is mainly used to define the values in it
+    def __init__(self, name, price, quantity):
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+    def calculate_total_price(self):
+        return self.price*self.quantity
         
 
 
-item1=item()
-item1.name="phone"
-item1.price=100
-item1.quantity=5
-print(item1.calculate_total_price(item1.price, item1.quantity))
+item1=item("phone", 100, 5)
 
-item2=item()
-item2.name="Laptop"
-item2.price=1000
-item2.quantity=3
-print(item2.calculate_total_price(item2.price, item2.quantity))
+item2=item("laptop",1000,3)
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
 
 
